33af73bcee3a6f2dcbc2886b7f58a8ac52cd11aa
KittikonDonde <kittikon1659@gmail.com>
2024-01-12 15:30:49 +0700

    add new

---
+++ index.js
@@ -0,0 +1,18 @@
+const oracledb = require('oracledb');
+const axios = require('axios');
+
+async function connectToOracle() {
+    let connection;
+
+   
+    
+    const dbConfig = {
+      user: 'your_username',
+      password: 'your_password',
+      connectString: 'your_connection_string'
+    };
+
+}
+
+// Call the function to connect to Oracle Database
+connectToOracle();
